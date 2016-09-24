9adbfa740462041030e015c1190172e3a568d386
yezhangyuan <mia5121@163.com>
2016-09-24 16:37:21 +0800

    first time update

---
+++ src/main/java/com.zaney/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.zaney.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yzy on 2016/9/24.
+ *
+ * @email mia5121@163.com
+ */
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String toIndex(){
+		return "/index";
+	}
+
+
+}
