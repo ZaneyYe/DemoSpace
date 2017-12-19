25191c5b8cf392854821fb53d735a7b37a76c4a5
yezhangyuan <mia5121@163.com>
2017-12-19 15:57:00 +0800

    config update

--- src/main/java/com/demo/api/en/Constants.java
+++ src/main/java/com/demo/api/en/Constants.java
@@ -1,5 +1,7 @@
 package com.demo.api.en;
 
+import com.demo.util.ConfigUtil;
+
 /**
  * Created by yzy on 2017/08/14 上午 10:11.
  * email: mia5121@163.com
@@ -8,9 +10,9 @@ public interface Constants {
 
 	String uploadBasePath = "http://192.168.43.3:80/var/www/html/image/";
 
-	String localShowPath = "http://localhost/";
+	String localShowPath = ConfigUtil.getConfigValue("localShowPath");
 
-	String localUploadPath = "E:\\JinXin\\pics\\";
+	String localUploadPath = ConfigUtil.getConfigValue("localUploadPath");
 
 	String REDIS_ARTICLE_KEY = "article_detail_";
 
---
+++ src/main/java/com/demo/util/ConfigUtil.java
@@ -0,0 +1,25 @@
+package com.demo.util;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by yzy on 2017/12/14.
+ * email: mia5121@163.com
+ */
+public class ConfigUtil {
+
+
+	public static String getConfigValue(String configKey) {
+		ResourceBundle bundle = ResourceBundle.getBundle("conf");
+		if (bundle.containsKey(configKey)) {
+			String value = bundle.getString(configKey);
+			return value;
+		}
+		return "";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getConfigValue("localShowPath"));
+	}
+
+}
